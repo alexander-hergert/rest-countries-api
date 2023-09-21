1447ace4bc8e519b67f0e7ff19b6b05e19420e5e
Alexander Hergert <alexander.hergert1989@gmail.com>
2023-09-21 15:09:39 +0200

    Add fonts and basic styles. Setup colors in tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkModeElements: "hsl(209, 23%, 22%)",
+        darkModeBackground: "hsl(207, 26%, 17%)",
+        lightModeText: "hsl(200, 15%, 8%)",
+        lightModeInput: "hsl(0, 0%, 52%)",
+        lightModeBackground: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
